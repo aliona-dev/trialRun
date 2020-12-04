5dfb8b01721cb0b5021c80fb3d4252ca083baba0
kitsune777 <watermonkey777gmail.com>
2020-12-04 12:46:46 -0800

    index added

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const axios = require("axios");
 
 const GHIPHY_API_BASE_URL = "http://api.giphy.com/v1/gifs";
-const SEARCH_KEYWORD = "fox";
+const SEARCH_WORD = "fox";
 
 axios
   .get(
-    `${GHIPHY_API_BASE_URL}/search?api_key=${process.env.GHIPHY_API_KEY}&q=${SEARCH_KEYWORD}`
+    `${GHIPHY_API_BASE_URL}/search?api_key=${process.env.GHIPHY_API_KEY}&q=${SEARCH_WORD}`
   )
   .then((res) => {
     const gifs = res.data.data;
