e526bcfb1112122ca3ba9621c8ad0dbe6b7877e1
kitsune777 <watermonkey777gmail.com>
2020-12-04 12:36:08 -0800

    hide api keys

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+
 const axios = require("axios");
 
-const GHIPHY_API_KEY = "H6IpbQtY6thbhUnfLOk3KbkUMaXiWmm7";
+// const GHIPHY_API_KEY = "H6IpbQtY6thbhUnfLOk3KbkUMaXiWmm7";
 const GHIPHY_API_BASE_URL = "http://api.giphy.com/v1/gifs";
 const SEARCH_KEYWORD = "fox";
 
 axios
   .get(
-    `${GHIPHY_API_BASE_URL}/search?api_key=${GHIPHY_API_KEY}&q=${SEARCH_KEYWORD}`
+    `${GHIPHY_API_BASE_URL}/search?api_key=${process.env.GHIPHY_API_KEY}&q=${SEARCH_KEYWORD}`
   )
   .then((res) => {
     const gifs = res.data.data;
